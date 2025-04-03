4b4c10f2fc67ca2b3acc8fa80c9973f793c4ea8c
Pritom07 <pritommohajon4545@gmail.com>
2025-04-03 20:32:13 +0600

    google and github login along with database update completed

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.Admin_PASS);
-
 const uri = `mongodb+srv://${process.env.Admin_DB}:${process.env.Admin_PASS}@cluster0.r5e76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -27,12 +25,14 @@ async function run() {
 
     const usersCollection = client.db("NaturaVoyage").collection("users");
 
+    //for signup
     app.post("/users", async (req, res) => {
       const newUser = req.body;
       const result = await usersCollection.insertOne(newUser);
       res.send(result);
     });
 
+    //for signin
     app.patch("/users", async (req, res) => {
       const signInUser = req.body;
       const userEmail = signInUser.email;
@@ -44,6 +44,52 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //for google
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const userName = user.name;
+      const userEmail = user.email;
+      const lastsignInTime = user.lastSignIn;
+      const filter = { email: userEmail };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: userName,
+          email: userEmail,
+          lastSignIn: lastsignInTime,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    //for github(two put operations using same '/users' ,to make identical use '/users/github')
+    app.put("/users/github", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const userName = user.name;
+      const lastsignInTime = user.lastSignInTime;
+      const filter = { name: userName };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: userName,
+          lastSignIn: lastsignInTime,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
